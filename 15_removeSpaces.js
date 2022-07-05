b491075bb4cef49f8af712644c722aa5d870f6e5
Juan Manuel Fernandez <juanmafc22@gmail.com>
2022-07-05 17:42:20 -0300

    16 and 17 solved 2022-05-07

---
+++ 15_removeSpaces.js
@@ -0,0 +1,31 @@
+// Solved 2022-07-05
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// Solution
+// 1. Iterate through the entire length of the x string
+// 2. For every char of the string that isn't an empty space,
+// add it to the result string;
+// 3. Return the result string;
+
+function noSpace(x){
+
+    let result = "";
+
+    for (let i=0; i<x.length; i++) {
+
+        if (x[i] != " ") {
+            result += x[i];
+        }
+    }
+
+    return result;
+
+}
+
+console.log(noSpace('8aaaaa dddd r     '));
+
+// Samples
+// ('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+// ('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+// ('8aaaaa dddd r     '), '8aaaaaddddr');
---
+++ 16_oddOrEven.js
@@ -0,0 +1,32 @@
+// Task: // Given a list of integers, determine whether the sum
+// of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Solution
+// 1. Use the redude method to add all the elements of array
+// 2. Return "odd" or "even" depending if the sume %2 is equal to zero
+
+function oddOrEven(array) {
+
+    let sum = array.reduce ((previous, current) => {
+        return previous + current;
+    } )
+    
+    sum %2 == 0 ? console.log("even") : console.log("odd");
+
+}
+
+console.log(oddOrEven([0, 1, 4]))
+
+// Samples
+
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
