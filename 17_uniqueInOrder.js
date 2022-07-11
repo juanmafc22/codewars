05484cdf32918bef975d894dbae20d5239c990a2
Juan Manuel Fernandez <juanmafc22@gmail.com>
2022-07-11 11:05:21 -0300

    started 17

---
+++ 17_uniqueInOrder.js
@@ -0,0 +1,20 @@
+// Implement the function unique_in_order which takes as
+// argument a sequence and returns a list of items without
+// any elements with the same value next to each other
+// and preserving the original order of elements.
+
+// For example:
+// ('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// ('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// ([1,2,2,3,3])       == [1,2,3]
+
+function  (iterable) {
+
+    console.log(iterable);
+
+}
+
+console.log(("juan"));
+
+Samples
+// ('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
